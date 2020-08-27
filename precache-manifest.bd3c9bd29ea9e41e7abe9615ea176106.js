af86beba4d9ccd1933492026fefc1f5ed755ce9b
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-08-27 13:03:01 -0500

    Updates

---
+++ precache-manifest.bd3c9bd29ea9e41e7abe9615ea176106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b465cb5f069fe32304b5df5784699a",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "68d90de3002d901248b8",
+    "url": "/imc-comfenalco/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af484823df962f90fdb0",
+    "url": "/imc-comfenalco/static/js/2.c7f2410b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.c7f2410b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d90de3002d901248b8",
+    "url": "/imc-comfenalco/static/js/main.9bba48c4.chunk.js"
+  },
+  {
+    "revision": "cf5ef7bb299543477c49",
+    "url": "/imc-comfenalco/static/js/runtime-main.e4874b84.js"
+  }
+]);
