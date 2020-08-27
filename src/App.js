8f711b06084803d9ee10ebd6bfcac2fc0e4dd7a2
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-08-27 12:38:39 -0500

    Primera subida

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+import Ciclodevida from './componentes/CicloDeVida'
+
+function App() {
+  return (
+    <div className="App">
+      <Ciclodevida />
+    </div>
+  );
+}
+
+export default App;
